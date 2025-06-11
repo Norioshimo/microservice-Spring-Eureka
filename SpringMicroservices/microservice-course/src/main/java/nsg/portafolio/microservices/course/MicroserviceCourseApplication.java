d9b301e38e6d42ed75686387539f7042a1f6a5a9
Norio <42630783+Norioshimo@users.noreply.github.com>
2025-06-11 20:26:08 -0300

    Configuración general en de micro servicios

--- SpringMicroservices/microservice-course/src/main/java/nsg/portafolio/microservices/course/MicroserviceCourseApplication.java
+++ SpringMicroservices/microservice-course/src/main/java/nsg/portafolio/microservices/course/MicroserviceCourseApplication.java
@@ -10,8 +10,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class MicroserviceCourseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceCourseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceCourseApplication.class, args);
+    }
 
 }
